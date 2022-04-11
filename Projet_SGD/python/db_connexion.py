ff1d328a7c9769e51930e8fbf06f5050eaa5a85e
ababacar gueye <sadikhabou@gmail.com>
2022-04-11 17:40:08 +0200

    réorganisation du code

--- Projet_SGD/python/db_connexion.py
+++ Projet_SGD/python/db_connexion.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-url="mongodb://br683616:br683616@mongo/?authSource=br683616&authMechanism=SCRAM-SHA-1"
+url="mongodb://ag425107:ag425107@mongo/?authSource=ag425107&authMechanism=SCRAM-SHA-1"
 c = MongoClient(url)
-db = c.br683616
+db = c.ag425107
--- db_connexion.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-from bson.objectid import ObjectId
-url="mongodb://ag425107:ag425107@mongo/?authSource=ag425107&authMechanism=SCRAM-SHA-1"
-c = MongoClient(url)
-db = c.ag425107
